2d2ae6b28ce237c8d8fc6ac3a8a543d54ba3afd3
Nihat Halici <nihat.halici@gmail.com>
2021-05-07 01:10:47 -0700

    Update Examples.pp

--- Configuration-Management-and-the-Cloud/Week-1/Puppet/Examples.pp
+++ Configuration-Management-and-the-Cloud/Week-1/Puppet/Examples.pp
@@ -32,3 +32,19 @@ class timezone {
     replace => true,
   }
 }
+
+
+
+class ntp {
+  package { 'ntp':
+    ensure => latest,
+  }
+  file { '/etc/ntp.conf':
+    source => 'puppet:///modules/ntp/ntp.conf'
+    replace => true,
+  }
+  service { 'ntp':
+    enable => true,
+    ensure => running,
+  }
+}
