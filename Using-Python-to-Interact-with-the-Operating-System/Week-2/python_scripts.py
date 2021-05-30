078f0a15ccdcd3a8733b0a84d910b1de0e739fc0
Nihat Halici <nihat.halici@gmail.com>
2021-05-30 07:31:32 -0700

    Update python_scripts.py

--- Using-Python-to-Interact-with-the-Operating-System/Week-2/python_scripts.py
+++ Using-Python-to-Interact-with-the-Operating-System/Week-2/python_scripts.py
@@ -271,3 +271,22 @@ with open('software.csv') as software:
     reader = csv.DictReader(software)
     for row in reader:
         print(("{} has {} users").format(row["name"], row["users"]))
+
+"""
+We call two different methods on the writer. The writeheader method 
+will create the first line of the CSV based on keys that we passed, 
+and the writerows method will turn the list of dictionaries into lines in that file.
+"""
+
+import csv
+
+users = [ {"name": "Sol Mansi", "username": "solm", "department": "IT"},
+          {"name": "Lio Nelson", "username": "lion", "department": "User Experience"},
+          {"name": "Charlie Grey", "username": "greyc", "department": "Development"}]
+
+keys = ["name", "username", "department"]
+
+with open('by_department.csv', 'w') as by_department:
+    writer = csv.DictWriter(by_department, fieldnames=keys)
+    writer.writeheader()
+    writer.writerows(users)
