dedaec7118d4453e3f035f4327633cb5606d62a3
Nihat Halici <nihat.halici@gmail.com>
2021-06-01 01:44:31 -0700

    Update python_scripts.py

--- Automating-Real-World-Tasks-with-Python/Week-2/python_scripts.py
+++ Automating-Real-World-Tasks-with-Python/Week-2/python_scripts.py
@@ -56,3 +56,30 @@ Now, we can record multiple phone numbers per person, and give them descriptive 
 This would be hard to store in a CSV file, because the data is not flat. 
 To help us with that, there's a bunch of different formats that we can use to store our data when the structure isn't flat.
 """
+
+#################################
+# Data Serialization Formats    #
+#################################
+
+
+"""
+JSON (JavaScript Object Notation) is the serialization format that we'll use the most in this course. 
+We'll go into some details later but, for now, let's just use the json module to convert our people list of dictionaries into JSON format.  
+"""
+
+import json
+
+with open('people.json', 'w') as people_json:
+    json.dump(people, people_json, indent=2)
+
+# This code uses the json.dump() function to serialize the people object into a JSON file. 
+
+"""
+YAML (Yet Another Markup Language) has a lot in common with JSON. They’re both formats that can be easily understood by a human 
+when looking at the contents. In this example, we’re using the yaml.safe_dump() method to serialize our object into YAML: 
+"""
+import yaml
+
+with open('people.yaml', 'w') as people_yaml:
+    yaml.safe_dump(people, people_yaml)
+ 
