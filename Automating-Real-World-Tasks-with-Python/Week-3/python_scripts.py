364a77922cfd5df5f86fad8b0f6eed2e9f10bf0a
Nihat Halici <nihat.halici@gmail.com>
2021-06-05 01:24:30 -0700

    Update python_scripts.py

--- Automating-Real-World-Tasks-with-Python/Week-3/python_scripts.py
+++ Automating-Real-World-Tasks-with-Python/Week-3/python_scripts.py
@@ -79,3 +79,62 @@ The email message as a whole has the MIME type "multipart/mixed".
 Each part of the message has its own MIME type. The message body is still there as a "text/plain" part, 
 and the image attachment is a "image/png" part. 
 """
+
+##########################
+# Sending the Email      #
+# Through an SMTP Server #
+##########################
+
+# We’ll send the message using the built-in smtplib Python module. Let's start by importing the module.
+import smtplib
+
+# Let's create a smtplib.SMTP object and try to connect to the local machine.
+mail_server = smtplib.SMTP('localhost')
+
+"""
+You can connect to a remote SMTP server using Transport Layer Security (TLS). An earlier version of the TLS protocol was called Secure Sockets Layer (SSL), 
+and you’ll sometimes see TLS and SSL used interchangeably. This SSL/TLS is the same protocol that's used to add a secure transmission layer to HTTP, 
+making it HTTPS. Within the smtplib, there are two classes for making connections to an SMTP server: The SMTP class will make a direct SMTP connection, 
+and the SMTP_SSL class will make a SMTP connection over SSL/TLS. Like this:
+"""
+mail_server = smtplib.SMTP_SSL('smtp.example.com')
+
+"""
+If you want to see the SMTP messages that are being sent back and forth by the smtplib module behind the scenes, you can set the debug level on the SMTP 
+or SMTP_SSL object. The examples in this lesson won’t show the debug output, but you might find it interesting!
+"""
+mail_server.set_debuglevel(1)
+
+"""
+Now that we’ve made a connection to the SMTP server, the next thing we need to do is authenticate to the SMTP server. Typically, email providers wants us 
+to provide a username and password to connect. Let's put the password into a variable so it's not visible on the screen.
+"""
+import getpass
+mail_pass = getpass.getpass('Password? ')
+
+"""
+The example above uses the getpass module so that passers-by won't see the password on the screen. 
+Watch out, though; the mail_pass variable is still just an ordinary string!
+"""
+print(mail_pass)
+
+# Now that we have the email user and password configured, we can authenticate to the email server using the SMTP object's login method.
+mail_server.login(sender, mail_pass)
+
+"""
+If the login attempt succeeds, the login method will return a tuple of the SMTP status code and a message explaining the reason for the status. 
+If the login attempt fails, the module will raise a SMTPAuthenticationError exception.
+
+If you wrote a script to send an email message, how would you handle this exception?
+
+Sending your message
+Alright! We're connected and authenticated to the SMTP server. Now, how do we send the message?
+"""
+mail_server.send_message(message)
+
+"""
+Okay, well that last bit was pretty easy! We did the hard part first! The send_message method returns a dictionary of any recipients that weren’t 
+able to receive the message. Our message was delivered successfully, so send_message returned an empty dictionary. 
+Finally, now that the email is sent, let's close the connection to the mail server.
+"""
+mail_server.quit()
